3ea17dd1bea78f5a8d10f30b37d8f2e0cfda565e
shumpei-noda <shumpei.noda@nextremer.com>
2017-10-23 15:11:17 +0900

    restaurant_count.pyに指定したjsonの中に存在する店の軒数を表示

--- restaurant_count.py
+++ restaurant_count.py
@@ -1,7 +1,16 @@
+import sys
 import json
 
+def count(paths):
+    for path in paths:
+        with open(path, 'r') as open_json:
+            tips_json = json.load(open_json)
+            print("restaurants: ", len(tips_json))
+            
 def main():
-    
+    if len(sys.argv) < 2:
+        print("jsonのpathくれ")
+    count(sys.argv[1:])
 
 if __name__ == "__main__":
     main()
