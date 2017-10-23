c69176603c2e1a8fd1148870c90da9e5fbe8bbcf
shumpei-noda <shumpei.noda@nextremer.com>
2017-10-23 15:37:20 +0900

    レストランの数の出力とカウントのプログラムを切り離した

--- restaurant_count.py
+++ restaurant_count.py
@@ -2,15 +2,23 @@ import sys
 import json
 
 def count(paths):
+    n_restaurant_list = []
     for path in paths:
         with open(path, 'r') as open_json:
             tips_json = json.load(open_json)
-            print(path, ":", len(tips_json), "restaurants")
+            n_restaurant_list += [len(tips_json)]
+
+    return n_restaurant_list
+
+def print_count(n_restaurant_list, paths):
+    for n_restaurant, path in zip(n_restaurant_list, paths):
+        print(path, ":", n_restaurant, "restaurants")
 
 def main():
     if len(sys.argv) < 2:
         print("jsonのpathくれ")
-    count(sys.argv[1:])
+    n_restaurant_list = count(sys.argv[1:])
+    print_count(n_restaurant_list, sys.argv[1:])
 
 if __name__ == "__main__":
     main()
