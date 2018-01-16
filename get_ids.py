7cac3f91178221f13ac26f1134cd7297bcde8ebe
shumpei-noda <shumpei.noda@nextremer.com>
2018-01-16 18:40:52 +0900

    叩くAPIURLの定数化と返り値に取得したvenue情報を追加

--- get_ids.py
+++ get_ids.py
@@ -4,28 +4,25 @@ import click
 import requests
 
 VERSION = '20170801'
+SEARCH_FOR_VENUE_URL = 'https://api.foursquare.com/v2/venues/search'
 
-def get_venue_id(search_parameter, tips_num_lower_limit):
+def get_venue_id(search_parameter):
     # foursquareのAPIを叩いて指定した条件でvenueIDをとってくる
-    search_for_venue_url = 'https://api.foursquare.com/v2/venues/search'
     search_for_venue_parameters = {'v': VERSION}
     for search_parameter_key in search_parameter:
         search_for_venue_parameters[search_parameter_key] = search_parameter[search_parameter_key]
 
     # idの取得前に取得条件を表示し、確認する
-    search_for_venue_response = requests.get(url=search_for_venue_url, params=search_for_venue_parameters)
+    search_for_venue_response = requests.get(url=SEARCH_FOR_VENUE_URL, params=search_for_venue_parameters)
     venue_data_json = json.loads(search_for_venue_response.text)
     if 'errorType' in venue_data_json['meta']:
-        # print("error: " + venue_data_json['meta']['errorDetail'])
-        return
-
+        raise Exception('spam', 'eggs')
     venue_data = venue_data_json['response']['venues']
     #下限よりも多くのtipsがあるvenueのIDを抽出
     venue_ids = {}
     for venue in venue_data:
         venue_ids[venue['name']] = venue['id']
-
-    return venue_ids
+    return venue_ids, search_for_venue_response.text
 
 # idの保存
 def save_ids_json(ids, path):
