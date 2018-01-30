835cbdd78dbf6f9e7777ea111b774924ae227d71
shumpei-noda <shumpei.noda@nextremer.com>
2018-01-30 21:14:19 +0900

    トランザクション用の関数を外部でも呼べるように関数化

--- tips_requests_table.py
+++ tips_requests_table.py
@@ -34,14 +34,23 @@ def get_join_venues():
     return rows
 
 def get_waiting_tasks():
-    rows = db.table('tips_requests').where('status', '=', '0').join('venues', 'tips_requests.venue_id', '=', 'venues.id').get()
+    rows = db.table('tips_requests').where('status', '=', '0').join('venues', 'tips_requests.venue_id', '=', 'venues.id').lock_for_update().get()
     return rows
 
 def get_waiting_task():
-    row = db.table('tips_requests').where('status', '=', '0').join('venues', 'tips_requests.venue_id', '=', 'venues.id').limit(1).get()
+    begin_transaction()
+    row = db.table('tips_requests').where('status', '=', '0').join('venues', 'tips_requests.venue_id', '=', 'venues.id').lock_for_update().first()
     return row
 
 def update_status(venue_id, status_code):
     db.table('tips_requests').where('venue_id', venue_id).update({'status': status_code,
                                                            'updated_at': datetime.datetime.today()
                                                           })
+def begin_transaction():
+    db.begin_transaction()
+
+def rollback():
+    db.rollback()
+
+def commit():
+    db.commit()
