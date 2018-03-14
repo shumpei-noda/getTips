b95fdaa34f9da95261b4f026e463788f71fca698
shumpei-noda <shumpei.noda@nextremer.com>
2018-03-14 13:13:16 +0900

    取得リクエストのテーブルを変更

--- tips_requests_table.py
+++ tips_requests_table.py
@@ -15,34 +15,36 @@ config = {
 }
 db = DatabaseManager(config)
 
+table_name = 'tips_requests2'
+
 def insert(venue_id, status_code):
-    if db.table('tips_requests').where('venue_id', venue_id).get():
+    if db.table(table_name).where('venue_id', venue_id).get():
         update_status(venue_id, status_code)
     else:
-        db.table('tips_requests').insert({'venue_id': venue_id,
+        db.table(table_name).insert({'venue_id': venue_id,
                                           'status': status_code,
                                           'created_at': datetime.datetime.today(),
                                           'updated_at': datetime.datetime.today()
                                          })
 
 def get():
-    rows = db.table('tips_requests').get()
+    rows = db.table(table_name).get()
     return rows
 
 def get_join_venues():
-    rows = db.table('tips_requests').join('venues', 'tips_requests.venue_id', '=', 'venues.id').get()
+    rows = db.table(table_name).join('venues', table_name+'.venue_id', '=', 'venues.id').get()
     return rows
 
 def get_waiting_tasks():
-    rows = db.table('tips_requests').where('status', '=', '0').join('venues', 'tips_requests.venue_id', '=', 'venues.id').lock_for_update().get()
+    rows = db.table(table_name).where('status', '=', '0').join('venues', table_name+'.venue_id', '=', 'venues.id').lock_for_update().get()
     return rows
 
 def get_waiting_task():
-    row = db.table('tips_requests').where('status', '=', '0').join('venues', 'tips_requests.venue_id', '=', 'venues.id').where('tip_count', '!=', '0').lock_for_update().first()
+    row = db.table(table_name).where('status', '=', '0').join('venues', table_name+'.venue_id', '=', 'venues.id').where('tip_count', '!=', '0').lock_for_update().first()
     return row
 
 def update_status(venue_id, status_code):
-    db.table('tips_requests').where('venue_id', venue_id).update({'status': status_code,
+    db.table(table_name).where('venue_id', venue_id).update({'status': status_code,
                                                                   'updated_at': datetime.datetime.today()
                                                                  })
 def begin_transaction():
